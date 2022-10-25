dbf7d6f5c19d2b9f47c18f763b86892d6756c6e7
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-10-24 21:51:19 -0400

    Updated models and tag routes

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findall({include: [{ model: Product }] });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  }
+
+);
+
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }] });
+
+  if (!tagData) {
+    res.status(404).json({ message: 'No location found with this id!' });
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
